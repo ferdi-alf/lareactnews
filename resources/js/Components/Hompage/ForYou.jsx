0636f89aa78fa1cba44d5ce480ef32e81051117b
ferdi-alf <ferdialfianferdi.08@gmail.com>
2024-06-01 17:50:06 +0700

    random news has been completed

--- resources/js/Components/Hompage/ForYou.jsx
+++ resources/js/Components/Hompage/ForYou.jsx
@@ -17,7 +17,7 @@ const isForYou = (data) => {
     }
 
     return (
-        <div className="w-full bg-white flex flex-col">
+        <div className="w-full bg-white flex flex-col overflow-x-auto">
             {data.slice(0, 3).map((news, i) => (
                 <Link key={i} className="rndm" onClick={() => handleViewNews(news.id)} href={route('view.berita', { id: news.id })} >
                     <figure>
@@ -26,13 +26,13 @@ const isForYou = (data) => {
                     <div className="flex card-desr flex-col items-center justify-evenly">
                         <div className="card-title flex flex-col">
                             <div className="flex flex-nowrap items-center justify-center">
-                                <p className="title">{news.title}</p>
+                                <p className="title hover:underline">{news.title}</p>
                                 <div className="badge bg-transparent view gap-1" style={{ color: "#555" }}>
                                     <FontAwesomeIcon icon={faEye} style={{ fontSize: "10px" }} />
                                     {news.views}
                                 </div>
                             </div>
-                            <p className="desc">{news.description}</p>
+                            <p className="desc hover:underline">{news.description}</p>
                             <div className="w-full flex justify-around items-center">
                                 <p className="carbonr">
                                     <HumanReadableTime timestamp={news.created_at} />
@@ -46,7 +46,67 @@ const isForYou = (data) => {
                     </div>
                 </Link>
             ))}
-        </div>
+            <div className="apa">
+                {data.slice(3, 10).map((news, i) => (
+                    <div className="knn">
+                        <Link
+                            key={i}
+                            onClick={() => handleViewNews(news.id)}
+                            href={route('view.berita', { id: news.id })}
+                        >
+                            <figure>
+                                <img src={`/storage/images/${news.foto}`} alt="Shoes" />
+                            </figure>
+                            <div className="card-desc gap-y-2 flex flex-col">
+                                <p className="title">{news.title}</p>
+                                <p className="desc">{news.description}</p>
+                            </div>
+                            <div className="w-full flex-col flex justify-center items-end">
+                                <p className="carbon">
+                                    <HumanReadableTime timestamp={news.created_at} />
+                                </p>
+                                <div className="flex  justify-between gap-x-2 items-center flex-wrap">
+                                    <div className="badge bg-transparent view gap-1" style={{ color: "#555" }}>
+                                        <FontAwesomeIcon icon={faEye} style={{ fontSize: "10px" }} />
+                                        {news.views}
+                                    </div>
+                                    <div style={{ color: "#555" }} className="badge name bg-white">{news.author}</div>
+                                    <div className="badge badge-secondary">{news.category}</div>
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+            {data.slice(10).map((news, i) => (
+                <Link key={i} className="rndm" style={{ marginTop: "7px" }} onClick={() => handleViewNews(news.id)} href={route('view.berita', { id: news.id })} >
+                    <figure>
+                        <img src={`/storage/images/${news.foto}`} alt="Shoes" />
+                    </figure>
+                    <div className="flex card-desr flex-col items-center justify-evenly">
+                        <div className="card-title flex flex-col">
+                            <div className="flex flex-nowrap items-center justify-center">
+                                <p className="title hover:underline">{news.title}</p>
+                                <div className="badge bg-transparent view gap-1" style={{ color: "#555" }}>
+                                    <FontAwesomeIcon icon={faEye} style={{ fontSize: "10px" }} />
+                                    {news.views}
+                                </div>
+                            </div>
+                            <p className="desc hover:underline">{news.description}</p>
+                            <div className="w-full flex justify-around items-center">
+                                <p className="carbonr">
+                                    <HumanReadableTime timestamp={news.created_at} />
+                                </p>
+                                <div className="flex justify-between gap-x-2 items-center flex-wrap">
+                                    <div style={{ color: "#555" }} className="badge name bg-white">{news.author}</div>
+                                    <div className="badge badge-secondary">{news.category}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            ))}
+        </div >
     )
 }
 
